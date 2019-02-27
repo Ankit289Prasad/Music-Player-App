d7c6dd8036d550e09171b21b065ad52fe0f5a95a
Ankit Prasad <ankit28prasad9@gmail.com>
2019-02-28 02:35:55 +0530

    SplashScreenAdded

--- app/src/main/java/com/developes/musicplayer/SplashScreen.java
+++ app/src/main/java/com/developes/musicplayer/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.developes.musicplayer;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this , MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        },3000);
     }
 }
